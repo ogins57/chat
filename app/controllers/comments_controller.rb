591410d6bcebbe737348717e6c1a7c26cb646cea
荻野 浩史 <cheese.cake0611@gmail.com>
2013-04-13 21:13:37 +0900

    add commnet function.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,31 +1,11 @@
 class CommentsController < ApplicationController
 
     def create
-        @post = Post.find(params[:post_id])
-        #@comment = Post.find(params[:post_id]).comments.create(params[:comment])
-        @comment = Post.find(params[:post_id]).comments.new(params[:comment])
-        if @comment.save
-            redirect_to post_path(@post)
-        else
-            render :template => "posts/show"
-        end
-    end
-
-
-    def destroy
-        @comment = Comment.find(params[:id])
-        @comment.destroy
-        render json: {comment: @comment}
-    end
-
-    def create
+        print session[:user].name
         @tweet = Tweet.find(params[:tweet_id])
-        @comment = Tweet.find(params[:tweet_id]).comments.new(params[:comment])
-        if @comment.save
-            redirect_to tweet_path(@tweet)
-        else
-            render :template => "tweets"
-        end
+        @comment = Tweet.find(params[:tweet_id]).comments.create(params[:comment])
+        @comment.user = @tweet.user
+        redirect_to tweets_path(@tweet)
     end
 
     def destroy
